b76407537001cf87434d4bab1086192269c69e8a
Miguel Ybañez <u20201b298@upc.edu.pe>
2023-09-24 16:26:18 -0500

    update technician

--- src/app/components/my-technicians/my-technicians.component.ts
+++ src/app/components/my-technicians/my-technicians.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TechnicianService } from 'src/app/services/technician.service';
 
 @Component({
   selector: 'app-my-technicians',
   templateUrl: './my-technicians.component.html',
   styleUrls: ['./my-technicians.component.css']
 })
-export class MyTechniciansComponent {
+export class MyTechniciansComponent implements OnInit{
+  technicians: any[] = [];
+  constructor(private technicianService: TechnicianService) { }
 
+  ngOnInit(): void{
+    this.loadTechnicians();
+  }
+
+  loadTechnicians(){
+    this.technicianService.getTechnician().subscribe(
+      (data) => {
+        this.technicians = data; // Assign the received data to the technicians property
+      },
+      (error) => {
+        console.error('Error fetching technicians:', error);
+      }
+    );
+  }
 }
--- src/app/services/technician.service.ts
+++ src/app/services/technician.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TechnicianService {
-  private apiUrl = '/api/users';
+  private apiUrl = '/api/technician';
 
   constructor(private http: HttpClient) { }
 
