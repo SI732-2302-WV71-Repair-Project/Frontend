e35a5ba36e8d0534c578d9028b8fa4428df19d26
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-09-26 17:26:16 -0500

    udpate login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'repair-project';
+  
+  public isUserLoggedIn: boolean = false;
+
+  constructor() { }
+
+  setLoginStatus(status: boolean):void{
+    this.isUserLoggedIn = status;
+  }
 }
