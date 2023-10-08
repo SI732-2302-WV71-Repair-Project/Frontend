d8315d703c4e9a0d5230e30da9dfa27571937680
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2023-10-07 21:11:03 -0500

    generating new device

--- src/app/components/new-device/new-device.component.ts
+++ src/app/components/new-device/new-device.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class NewDeviceComponent {
   deviceBrand = '';
+  deviceModel = '';
   deviceType = '';
   problemDescription = '';
   showConfirmation = false;
